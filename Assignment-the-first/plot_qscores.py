2b1369d8f9bf39b043019f30b3e73807c1777894
seanwho42 <seanwho42@uoregon.edu>
2025-07-25 11:51:00 -0700

    graphs finished

--- Assignment-the-first/plot_qscores.py
+++ Assignment-the-first/plot_qscores.py
@@ -4,29 +4,33 @@ import gzip
 import os
 from matplotlib import pyplot as plt
 import bioinfo
-from tqdm import tqdm
 
 
-# for testing
-fq_paths_lengths = [
-    ('test_R1.fq.gz', 12),
-    ('test_R2.fq.gz', 12),
-    ('test_R3.fq.gz', 12),
-    ('test_R4.fq.gz', 12)
+# # for testing
+# fq_paths_lengths = [
+#     ('test_R1.fq.gz', 12),
+#     ('test_R2.fq.gz', 12),
+#     ('test_R3.fq.gz', 12),
+#     ('test_R4.fq.gz', 12)
+# ]
+
+# for real data
+# list of tuples with (path, number of reads)
+fq_paths_n_reads = [
+    ('/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz', 363246735),
+    ('/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz', 363246735),
+    ('/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz', 363246735),
+    ('/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz', 363246735)
 ]
-
-# # for real data
-# data = ''
 
 
 def main():
-    for read_num, (file, length) in enumerate(fq_paths_lengths):
+    for read_num, (file, n_reads) in enumerate(fq_paths_n_reads):
 
         # progress bar initialization
-        milestone = length // 10
         print(f'Reading {file}')
         with gzip.open(file,'rt') as f:
-            for i, line in tqdm(enumerate(f)):
+            for i, line in enumerate(f):
                 # print(f.read())
                 # print(type(line))
                 if i % 4 == 3:
@@ -40,7 +44,7 @@ def main():
                     for j, char in enumerate(line):
                         # print(f'j: {j}')
                         # print(f'char: {char}')
-                        means[j] += bioinfo.convert_phred(char)/12
+                        means[j] += bioinfo.convert_phred(char)/n_reads
         # plot the thing here
         plt.bar(range(0,num_chars), means)
         plt.title("Mean Quality Score by Base Pair Index")
